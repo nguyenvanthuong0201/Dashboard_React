2680be8b5a7004bf6d23745fe6c6e13d5d6e2747
ThuongNguyen <thuongnv@hp-backup.com>
2023-04-26 18:14:22 +0700

    option image cloud

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,13 +1,15 @@
 import cloudinary from 'cloudinary';
-export const uploadImageCloud = async (imagePath) => {
+export const uploadImageCloud = async (imagePath, key = '') => {
     const options = {
         use_filename: true,
         unique_filename: false,
         overwrite: true,
         folder: "Life",
-        width: 150,
-        crop: "scale",
     };
+    if (key != '') {
+        options.width = 250;
+        options.crop = 'scale';
+    }
     try {
         // Upload the image
         const result = await cloudinary.v2.uploader.upload(imagePath, options);
